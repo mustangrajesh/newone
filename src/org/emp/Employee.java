0cbcfccb04332d5eca925d2f6df0d7575ffe6424
mustangrajesh <rajesh.9786621415@gmail.com>
2022-01-09 13:39:38 +0530

    updated code

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,39 @@
+package org.emp;
+
+public class Employee
+{
+	public void empId()
+	{
+		System.out.println("4646");
+	}
+	public void empName()
+	{
+		System.out.println("rajesh");
+	}
+	public void empPhone()
+	{
+		System.out.println("83264545");
+	}
+	public void empEmail()
+	{
+		System.out.println("rahdjb@gmail.com");
+	}
+	public void empAddress() 
+	{
+		System.out.println("bharthi nagar");
+
+	}
+	public static void main(String[] args) {
+		Employee her=new Employee();
+		{
+				her.empId();
+				her.empName();
+				her.empPhone();
+				her.empEmail();
+				her.empAddress();
+				
+		}
+
+
+	}
+}
